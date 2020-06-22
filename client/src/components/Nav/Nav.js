cf86536779c37fd52dc7d8127451fa0c34af91ed
Nicholas <nicxzmiller2@gmail.com>
2020-06-22 20:49:56 +0200

    login page and signup page

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -21,6 +21,9 @@ function Nav() {
                 <Link style={navStyle} to="/login">
                     <li>Login</li>
                 </Link>
+                <Link style={navStyle} to="/signup">
+                    <li>Sign Up</li>
+                </Link>
             </ul>
         </nav>
     );
